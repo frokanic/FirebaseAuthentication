8faeafa356d0e0f7c650a7f72dae1ac366f0303f
frokanic <f.rokanis@gmail.com>
2022-09-18 02:27:51 +0300

    Connected the project to firebase

--- app/src/main/java/com/example/firebaseauthentication/MainActivity.kt
+++ app/src/main/java/com/example/firebaseauthentication/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.firebaseauthentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.firebaseauthentication.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        changeFragment(FragmentLogin())
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply{
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
     }
 }
